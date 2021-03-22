dc8d6f97600db25e5ba8c014be91cc58f94c2691
Philip <philip.nuzhnyy@gmail.com>
2021-03-22 21:41:34 +0000

    tweak server runner when on render

--- app/server.py
+++ app/server.py
@@ -1,7 +1,11 @@
+import os
 import sys
 import uvicorn
 from runner import app
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run("server:app", host='0.0.0.0', port=5000, reload=True, log_level="info")
+        if os.getenv('ON_RENDER') is not None:
+            uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        else:
+            uvicorn.run("server:app", host='0.0.0.0', port=5000, reload=True, log_level="info")
